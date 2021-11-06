74261e62d1685a610510ba2a912ad8ec0b6324d5
Desuwa <desuwa@desuwa.net>
2021-11-05 19:02:41 -0700

    Minor performance improvements by switching to chunks_exact_mut

--- src/processing/resample.rs
+++ src/processing/resample.rs
@@ -1,6 +1,6 @@
-use image::{GenericImageView, ImageBuffer, Pixel, Primitive, Rgba32FImage};
+use image::{GenericImageView, ImageBuffer, Pixel, Primitive, Rgba32FImage, RgbaImage};
 use num_traits::{clamp, NumCast, ToPrimitive};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use rayon::iter::{IntoParallelIterator, ParallelBridge, ParallelIterator};
 
 // The MIT License (MIT)
 //
@@ -249,32 +249,28 @@ fn linear_to_srgb(s: f32) -> f32 {
     }
 }
 
+
 // Sample the rows of the supplied image using the provided filter.
 // The height of the image remains unchanged.
 // ```new_width``` is the desired width of the new image
 // ```filter``` is the filter to use for sampling.
 // ```image``` is not necessarily Rgba and the order of channels is passed through.
-fn horizontal_par_sample<P, S>(
-    image: &Rgba32FImage,
-    new_width: u32,
-    filter: &mut Filter,
-) -> ImageBuffer<P, Vec<S>>
-where
-    P: Pixel<Subpixel = S> + Send + 'static,
-    S: Primitive + Send + 'static,
-{
+fn horizontal_par_sample(image: &Rgba32FImage, new_width: u32, filter: &mut Filter) -> RgbaImage {
     let (width, height) = image.dimensions();
 
-    let max: f32 = NumCast::from(S::DEFAULT_MAX_VALUE).unwrap();
-    let min: f32 = NumCast::from(S::DEFAULT_MIN_VALUE).unwrap();
+    let max: f32 = NumCast::from(u8::DEFAULT_MAX_VALUE).unwrap();
+    let min: f32 = NumCast::from(u8::DEFAULT_MIN_VALUE).unwrap();
     let ratio = width as f32 / new_width as f32;
     let sratio = if ratio < 1.0 { 1.0 } else { ratio };
     let src_support = filter.support * sratio;
 
-    let out_vec: Vec<Vec<_>> = (0..new_width)
-        .into_par_iter()
-        .map(|outx| {
-            let mut outcol = Vec::with_capacity(height as usize);
+    // Create a rotated image and fix it later
+    let mut out = ImageBuffer::new(height, new_width);
+
+    out.chunks_exact_mut((height * 4) as usize)
+        .enumerate()
+        .par_bridge()
+        .for_each(|(outx, outcol)| {
             // Find the point in the input image corresponding to the centre
             // of the current pixel in the output image.
             let inputx = (outx as f32 + 0.5) * ratio;
@@ -308,134 +304,45 @@ where
             }
             ws.iter_mut().for_each(|w| *w /= sum);
 
-            for y in 0..height {
-                let mut t = (0.0, 0.0, 0.0, 0.0);
-
-                for (i, w) in ws.iter().enumerate() {
-                    let p = image.get_pixel(left + i as u32, y);
-
-                    #[allow(deprecated)]
-                    let vec = p.channels4();
-
-                    t.0 += vec.0 * w;
-                    t.1 += vec.1 * w;
-                    t.2 += vec.2 * w;
-                    t.3 += vec.3 * w;
-                }
-
-                t.0 = linear_to_srgb(t.0) * max;
-                t.1 = linear_to_srgb(t.1) * max;
-                t.2 = linear_to_srgb(t.2) * max;
-
-                #[allow(deprecated)]
-                let t = Pixel::from_channels(
-                    NumCast::from(FloatNearest(clamp(t.0, min, max))).unwrap(),
-                    NumCast::from(FloatNearest(clamp(t.1, min, max))).unwrap(),
-                    NumCast::from(FloatNearest(clamp(t.2, min, max))).unwrap(),
-                    NumCast::from(FloatNearest(clamp(t.3, min, max))).unwrap(),
-                );
-
-                outcol.push(t);
-            }
             outcol
-        })
-        .collect();
+                .chunks_exact_mut(4)
+                .enumerate()
+                .for_each(|(y, chunk)| {
+                    let mut t = (0.0, 0.0, 0.0, 0.0);
 
-    ImageBuffer::from_fn(new_width, height, |x, y| out_vec[x as usize][y as usize])
+                    for (i, w) in ws.iter().enumerate() {
+                        let p = image.get_pixel(left + i as u32, y as u32);
+
+                        #[allow(deprecated)]
+                        let vec = p.channels4();
+
+                        t.0 += vec.0 * w;
+                        t.1 += vec.1 * w;
+                        t.2 += vec.2 * w;
+                        t.3 += vec.3 * w;
+                    }
+
+                    t.0 = linear_to_srgb(t.0) * max;
+                    t.1 = linear_to_srgb(t.1) * max;
+                    t.2 = linear_to_srgb(t.2) * max;
+
+                    let t = (
+                        NumCast::from(FloatNearest(clamp(t.0, min, max))).unwrap(),
+                        NumCast::from(FloatNearest(clamp(t.1, min, max))).unwrap(),
+                        NumCast::from(FloatNearest(clamp(t.2, min, max))).unwrap(),
+                        NumCast::from(FloatNearest(clamp(t.3, min, max))).unwrap(),
+                    );
+
+                    chunk[0] = t.0;
+                    chunk[1] = t.1;
+                    chunk[2] = t.2;
+                    chunk[3] = t.3;
+                });
+        });
+
+    ImageBuffer::from_fn(new_width, height, |x, y| out[(y, x)])
 }
 
-
-// Sample the rows of the supplied image using the provided filter.
-// The height of the image remains unchanged.
-// ```new_width``` is the desired width of the new image
-// ```filter``` is the filter to use for sampling.
-// ```image``` is not necessarily Rgba and the order of channels is passed through.
-fn horizontal_sample<P, S>(
-    image: &Rgba32FImage,
-    new_width: u32,
-    filter: &mut Filter,
-) -> ImageBuffer<P, Vec<S>>
-where
-    P: Pixel<Subpixel = S> + 'static,
-    S: Primitive + 'static,
-{
-    let (width, height) = image.dimensions();
-    let mut out = ImageBuffer::new(new_width, height);
-    let mut ws = Vec::new();
-
-    let max: f32 = NumCast::from(S::DEFAULT_MAX_VALUE).unwrap();
-    let min: f32 = NumCast::from(S::DEFAULT_MIN_VALUE).unwrap();
-    let ratio = width as f32 / new_width as f32;
-    let sratio = if ratio < 1.0 { 1.0 } else { ratio };
-    let src_support = filter.support * sratio;
-
-    for outx in 0..new_width {
-        // Find the point in the input image corresponding to the centre
-        // of the current pixel in the output image.
-        let inputx = (outx as f32 + 0.5) * ratio;
-
-        // Left and right are slice bounds for the input pixels relevant
-        // to the output pixel we are calculating.  Pixel x is relevant
-        // if and only if (x >= left) && (x < right).
-
-        // Invariant: 0 <= left < right <= width
-
-        let left = (inputx - src_support).floor() as i64;
-        let left = clamp(left, 0, <i64 as From<_>>::from(width) - 1) as u32;
-
-        let right = (inputx + src_support).ceil() as i64;
-        let right = clamp(
-            right,
-            <i64 as From<_>>::from(left) + 1,
-            <i64 as From<_>>::from(width),
-        ) as u32;
-
-        // Go back to left boundary of pixel, to properly compare with i
-        // below, as the kernel treats the centre of a pixel as 0.
-        let inputx = inputx - 0.5;
-
-        ws.clear();
-        let mut sum = 0.0;
-        for i in left..right {
-            let w = (filter.kernel)((i as f32 - inputx) / sratio);
-            ws.push(w);
-            sum += w;
-        }
-        ws.iter_mut().for_each(|w| *w /= sum);
-
-        for y in 0..height {
-            let mut t = (0.0, 0.0, 0.0, 0.0);
-
-            for (i, w) in ws.iter().enumerate() {
-                let p = image.get_pixel(left + i as u32, y);
-
-                #[allow(deprecated)]
-                let vec = p.channels4();
-
-                t.0 += vec.0 * w;
-                t.1 += vec.1 * w;
-                t.2 += vec.2 * w;
-                t.3 += vec.3 * w;
-            }
-
-            t.0 = linear_to_srgb(t.0) * max;
-            t.1 = linear_to_srgb(t.1) * max;
-            t.2 = linear_to_srgb(t.2) * max;
-
-            #[allow(deprecated)]
-            let t = Pixel::from_channels(
-                NumCast::from(FloatNearest(clamp(t.0, min, max))).unwrap(),
-                NumCast::from(FloatNearest(clamp(t.1, min, max))).unwrap(),
-                NumCast::from(FloatNearest(clamp(t.2, min, max))).unwrap(),
-                NumCast::from(FloatNearest(clamp(t.3, min, max))).unwrap(),
-            );
-
-            out.put_pixel(outx, y, t);
-        }
-    }
-
-    out
-}
 
 // Sample the columns of the supplied image using the provided filter.
 // The width of the image remains unchanged.
@@ -456,11 +363,12 @@ where
     let sratio = if ratio < 1.0 { 1.0 } else { ratio };
     let src_support = filter.support * sratio;
 
-    let out_vec = (0..new_height)
-        .into_par_iter()
-        .map(|outy| {
-            let mut outrow = Vec::with_capacity((width * 4) as usize);
+    let mut out = ImageBuffer::new(width, new_height);
 
+    out.chunks_exact_mut((width * 4) as usize)
+        .enumerate()
+        .par_bridge()
+        .for_each(|(outy, outrow)| {
             // For an explanation of this algorithm, see the comments
             // in horizontal_sample.
             let inputy = (outy as f32 + 0.5) * ratio;
@@ -486,147 +394,55 @@ where
             }
             ws.iter_mut().for_each(|w| *w /= sum);
 
-            for x in 0..width {
-                let mut t = (0.0, 0.0, 0.0, 0.0);
-
-                for (i, w) in ws.iter().enumerate() {
-                    let p = image.get_pixel(x, left + i as u32);
-
-                    #[allow(deprecated)]
-                    let (k1, k2, k3, k4) = p.channels4();
-                    let vec: (f32, f32, f32, f32) = (
-                        NumCast::from(k1).unwrap(),
-                        NumCast::from(k2).unwrap(),
-                        NumCast::from(k3).unwrap(),
-                        NumCast::from(k4).unwrap(),
-                    );
-
-                    let linear = (
-                        srgb_to_linear(vec.0 / max),
-                        srgb_to_linear(vec.1 / max),
-                        srgb_to_linear(vec.2 / max),
-                    );
-
-                    t.0 += linear.0 * w;
-                    t.1 += linear.1 * w;
-                    t.2 += linear.2 * w;
-                    t.3 += vec.3 * w;
-                }
-
-                outrow.push(t.0);
-                outrow.push(t.1);
-                outrow.push(t.2);
-                outrow.push(t.3);
-            }
             outrow
-        })
-        .flatten()
-        .collect();
+                .chunks_exact_mut(4)
+                .enumerate()
+                .for_each(|(x, chunk)| {
+                    let mut t = (0.0, 0.0, 0.0, 0.0);
 
-    ImageBuffer::from_vec(width, new_height, out_vec).unwrap()
-}
+                    for (i, w) in ws.iter().enumerate() {
+                        let p = image.get_pixel(x as u32, left + i as u32);
 
-// Sample the columns of the supplied image using the provided filter.
-// The width of the image remains unchanged.
-// ```new_height``` is the desired height of the new image
-// ```filter``` is the filter to use for sampling.
-// The return value is not necessarily Rgba, the underlying order of channels in ```image``` is
-// preserved.
-fn vertical_sample<I, P, S>(image: &I, new_height: u32, filter: &mut Filter) -> Rgba32FImage
-where
-    I: GenericImageView<Pixel = P>,
-    P: Pixel<Subpixel = S> + 'static,
-    S: Primitive + 'static,
-{
-    let (width, height) = image.dimensions();
-    let mut out = ImageBuffer::new(width, new_height);
-    let mut ws = Vec::new();
+                        #[allow(deprecated)]
+                        let (k1, k2, k3, k4) = p.channels4();
+                        let vec: (f32, f32, f32, f32) = (
+                            NumCast::from(k1).unwrap(),
+                            NumCast::from(k2).unwrap(),
+                            NumCast::from(k3).unwrap(),
+                            NumCast::from(k4).unwrap(),
+                        );
 
-    let max: f32 = NumCast::from(S::DEFAULT_MAX_VALUE).unwrap();
-    let ratio = height as f32 / new_height as f32;
-    let sratio = if ratio < 1.0 { 1.0 } else { ratio };
-    let src_support = filter.support * sratio;
+                        let linear = (
+                            srgb_to_linear(vec.0 / max),
+                            srgb_to_linear(vec.1 / max),
+                            srgb_to_linear(vec.2 / max),
+                        );
 
-    for outy in 0..new_height {
-        // For an explanation of this algorithm, see the comments
-        // in horizontal_sample.
-        let inputy = (outy as f32 + 0.5) * ratio;
+                        t.0 += linear.0 * w;
+                        t.1 += linear.1 * w;
+                        t.2 += linear.2 * w;
+                        t.3 += vec.3 * w;
+                    }
 
-        let left = (inputy - src_support).floor() as i64;
-        let left = clamp(left, 0, <i64 as From<_>>::from(height) - 1) as u32;
-
-        let right = (inputy + src_support).ceil() as i64;
-        let right = clamp(
-            right,
-            <i64 as From<_>>::from(left) + 1,
-            <i64 as From<_>>::from(height),
-        ) as u32;
-
-        let inputy = inputy - 0.5;
-
-        ws.clear();
-        let mut sum = 0.0;
-        for i in left..right {
-            let w = (filter.kernel)((i as f32 - inputy) / sratio);
-            ws.push(w);
-            sum += w;
-        }
-        ws.iter_mut().for_each(|w| *w /= sum);
-
-        for x in 0..width {
-            let mut t = (0.0, 0.0, 0.0, 0.0);
-
-            for (i, w) in ws.iter().enumerate() {
-                let p = image.get_pixel(x, left + i as u32);
-
-                #[allow(deprecated)]
-                let (k1, k2, k3, k4) = p.channels4();
-                let vec: (f32, f32, f32, f32) = (
-                    NumCast::from(k1).unwrap(),
-                    NumCast::from(k2).unwrap(),
-                    NumCast::from(k3).unwrap(),
-                    NumCast::from(k4).unwrap(),
-                );
-
-                let linear = (
-                    srgb_to_linear(vec.0 / max),
-                    srgb_to_linear(vec.1 / max),
-                    srgb_to_linear(vec.2 / max),
-                );
-
-                t.0 += linear.0 * w;
-                t.1 += linear.1 * w;
-                t.2 += linear.2 * w;
-                t.3 += vec.3 * w;
-            }
-
-
-            #[allow(deprecated)]
-            // This is not necessarily Rgba.
-            let t = Pixel::from_channels(t.0, t.1, t.2, t.3);
-
-            out.put_pixel(x, outy, t);
-        }
-    }
+                    chunk[0] = t.0;
+                    chunk[1] = t.1;
+                    chunk[2] = t.2;
+                    chunk[3] = t.3;
+                });
+        });
 
     out
 }
 
-
 /// Resize the supplied image to the specified dimensions in linear light, assuming srgb input.
 /// ```nwidth``` and ```nheight``` are the new dimensions.
 /// ```filter``` is the sampling filter to use.
-pub fn resize_par_linear<I: GenericImageView>(
-    image: &I,
+pub fn resize_par_linear(
+    image: &RgbaImage,
     nwidth: u32,
     nheight: u32,
     filter: FilterType,
-) -> ImageBuffer<I::Pixel, Vec<<I::Pixel as Pixel>::Subpixel>>
-where
-    I::Pixel: Send + 'static,
-    <I::Pixel as Pixel>::Subpixel: Send + 'static,
-    I: std::marker::Sync,
-{
+) -> RgbaImage {
     let mut method = match filter {
         FilterType::Nearest => Filter {
             kernel: Box::new(box_kernel),
@@ -653,46 +469,4 @@ where
     // Note: tmp is not necessarily actually Rgba
     let tmp: Rgba32FImage = vertical_par_sample(image, nheight, &mut method);
     horizontal_par_sample(&tmp, nwidth, &mut method)
-}
-
-/// Resize the supplied image to the specified dimensions in linear light, assuming srgb input.
-/// ```nwidth``` and ```nheight``` are the new dimensions.
-/// ```filter``` is the sampling filter to use.
-pub fn resize_linear<I: GenericImageView>(
-    image: &I,
-    nwidth: u32,
-    nheight: u32,
-    filter: FilterType,
-) -> ImageBuffer<I::Pixel, Vec<<I::Pixel as Pixel>::Subpixel>>
-where
-    I::Pixel: Send + 'static,
-    <I::Pixel as Pixel>::Subpixel: Send + 'static,
-    I: std::marker::Sync,
-{
-    let mut method = match filter {
-        FilterType::Nearest => Filter {
-            kernel: Box::new(box_kernel),
-            support: 0.0,
-        },
-        FilterType::Triangle => Filter {
-            kernel: Box::new(triangle_kernel),
-            support: 1.0,
-        },
-        FilterType::CatmullRom => Filter {
-            kernel: Box::new(catmullrom_kernel),
-            support: 2.0,
-        },
-        FilterType::Gaussian => Filter {
-            kernel: Box::new(gaussian_kernel),
-            support: 3.0,
-        },
-        FilterType::Lanczos3 => Filter {
-            kernel: Box::new(lanczos3_kernel),
-            support: 3.0,
-        },
-    };
-
-    // Note: tmp is not necessarily actually Rgba
-    let tmp: Rgba32FImage = vertical_sample(image, nheight, &mut method);
-    horizontal_sample(&tmp, nwidth, &mut method)
 }
