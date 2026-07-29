d8a1cb71744966e87d0a5abb2bb4abfb7e3790e2
Desuwa <desuwa@desuwa.net>
2026-07-29 14:01:27 -0700

    Upate cron script

--- linux/random-cron.sh
+++ linux/random-cron.sh
@@ -2,28 +2,36 @@
 # Runs wallpapers random if the session is unlocked and if mpv is not playing.
 # Connects to the first session owned by the current user if DISPLAY is not set.
 
-lockname='i3lock'
+locks='i3lock swaylock hyprlock'
 
 mpvsocket='/tmp/mpvsocket'
 
 # In case you're using https://github.com/wis/mpvSockets or similar.
 mpv_glob='/tmp/mpv-sockets/*'
 
+# Set to true for wayland
+wayland=false
+
 #############
 
-pidof "$lockname" > /dev/null && exit 0
+echo "$locks" | xargs -r pidof > /dev/null && exit 0
 
 if [ -S "$mpvsocket" ]; then
-  paused=$(echo '{ "command": ["get_property", "pause"] }' | nc -U "$mpvsocket" 2>/dev/null | jq '.data')
+  paused=$(echo '{ "command": ["get_property", "pause"] }' | nc -UN "$mpvsocket" 2>/dev/null | jq '.data')
   [ "$paused" = "false" ] && exit 0
 fi
 
 for sock in $mpv_glob; do
   if [ -S "$sock" ]; then
-    paused=$(echo '{ "command": ["get_property", "pause"] }' | nc -U "$sock" 2>/dev/null | jq '.data')
+    paused=$(echo '{ "command": ["get_property", "pause"] }' | nc -UN "$sock" 2>/dev/null | jq '.data')
     [ "$paused" = "false" ] && exit 0
   fi
 done
+
+if [ "$wayland" = true ]; then
+  pkill -x -H -USR1 wallpapers
+  exit 0
+fi
 
 if [ -z "$DISPLAY" ]; then
   export DISPLAY=$(ps e ww -u "$USER" | sed -rn 's/.* DISPLAY=(:[0-9]+).*/\1/p' | sort | uniq | head -n 1)
